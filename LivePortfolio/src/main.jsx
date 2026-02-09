567c3b578b4f701c569bd6eb3731fdb8628a9422
KSHITIJ SAXENA <133942471+kssaxena@users.noreply.github.com>
2026-02-09 22:09:17 +0530

    feat: implement smooth scrolling functionality with Lenis integration

--- LivePortfolio/src/main.jsx
+++ LivePortfolio/src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
+import SmoothScrollProvider from "./components/SmoothScrollProvider.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
-      <App />
+      <SmoothScrollProvider>
+        <App />
+      </SmoothScrollProvider>
     </Router>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
